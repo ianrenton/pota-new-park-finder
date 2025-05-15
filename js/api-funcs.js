724817f60e064aad5ca837c2e9df63b1c24838dd
Ian Renton <ian@ianrenton.com>
2025-05-15 13:19:46 +0100

    Don't remove existing park markers when loading new ones, so you can still refer back to them.

--- js/api-funcs.js
+++ js/api-funcs.js
@@ -5,9 +5,6 @@
 // Main method to kick of querying the POTA API when the button is clicked or automatically
 // on load.
 function callAPI() {
-    // Clear existing data
-    parks.clear();
-    parks.length = 0;
     $("span#potaApiStatus").html("<i class='fa-solid fa-hourglass-half'></i> Loading...");
     // Get the bounds of the map for the query
     let bounds = map.getBounds();
@@ -44,42 +41,45 @@ function handlePOTAData(result, bounds) {
     let i = 0;
     parkUpdate.get("features").forEach(park => {
         const ref = park.properties.reference;
-        const newPark = {
-            uid: ref,
-            ref: ref,
-            name: park.properties.name,
-            lat: park.geometry.coordinates[1],
-            lon: park.geometry.coordinates[0],
-            activationChecked: false,
-            activated: false,
-            activatedByMe: false,
-            activationCount: 0,
-            activationByMeCount: 0,
-            lastActivationDate: null,
-            lastActivationCallsign: null,
-            lastActivationByMeDate: null,
-        };
-        // Bounds check before we populate the internal data model. POTA is not very strict on only returning parks inside the bounding
-        // box we asked for, so to avoid excess clutter on our own map, we check the bounds here and only add ones that are *really*
-        // on our screen.
-        if (newPark.lat >= bounds.getSouth() && newPark.lat <= bounds.getNorth() && newPark.lon >= bounds.getWest() && newPark.lon <= bounds.getEast()) {
-            parks.set(ref, newPark);
+        // If we don't already have data for this park, unpack the data.
+        if (!parks.has(ref)) {
+            const newPark = {
+                uid: ref,
+                ref: ref,
+                name: park.properties.name,
+                lat: park.geometry.coordinates[1],
+                lon: park.geometry.coordinates[0],
+                activationChecked: false,
+                activated: false,
+                activatedByMe: false,
+                activationCount: 0,
+                activationByMeCount: 0,
+                lastActivationDate: null,
+                lastActivationCallsign: null,
+                lastActivationByMeDate: null,
+            };
+            // Bounds check before we populate the internal data model. POTA is not very strict on only returning parks inside the bounding
+            // box we asked for, so to avoid excess clutter on our own map, we check the bounds here and only add ones that are *really*
+            // on our screen.
+            if (newPark.lat >= bounds.getSouth() && newPark.lat <= bounds.getNorth() && newPark.lon >= bounds.getWest() && newPark.lon <= bounds.getEast()) {
+                parks.set(ref, newPark);
 
-            // Now we need to fetch the park's list of activators to see if it's ever been activated, and activated by us. We do this
-            // asynchronously, with a delay to avoid overloading the API. The net effect is that markers will colour in slowly over the
-            // course of a few seconds.
-            setTimeout(function() {
-                $.ajax({
-                    url: POTA_ACTIVATIONS_URL + ref + "?count=all",
-                    dataType: 'json',
-                    timeout: 10000,
-                    success: async function (result) {
-                        if (result != null) {
-                            updateParkStatus(ref, result);
+                // Now we need to fetch the park's list of activators to see if it's ever been activated, and activated by us. We do this
+                // asynchronously, with a delay to avoid overloading the API. The net effect is that markers will colour in slowly over the
+                // course of a few seconds.
+                setTimeout(function () {
+                    $.ajax({
+                        url: POTA_ACTIVATIONS_URL + ref + "?count=all",
+                        dataType: 'json',
+                        timeout: 10000,
+                        success: async function (result) {
+                            if (result != null) {
+                                updateParkStatus(ref, result);
+                            }
                         }
-                    }
-                });
-            }, i++ * API_CALL_RATE_LIMIT_MILLIS);
+                    });
+                }, i++ * API_CALL_RATE_LIMIT_MILLIS);
+            }
         }
     });
 }
