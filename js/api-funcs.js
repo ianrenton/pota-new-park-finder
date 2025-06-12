642952583c21fda072596b07edf401b57570de0c
Ian Renton <ian@ianrenton.com>
2025-06-12 20:16:59 +0100

    Fix a bug where your failed actuations would still be counted towards the activation count and last activator.

--- js/api-funcs.js
+++ js/api-funcs.js
@@ -86,32 +86,40 @@ function handlePOTAData(result, bounds) {
 
 // Update the park's status when more info is retrieved from the API.
 function updateParkStatus(uid, apiResponse) {
-    let activations = objectToMap(apiResponse);
+    let activationAttempts = apiResponse;
+    // The API gives us all activation attempts including failed ones, so first of all we want to strip out any with
+    // <10 QSOs.
+    let activations = activationAttempts.filter(aa => aa.totalQSOs >= 10);
+
+    // Now grab the park to update
     let updatePark = parks.get(uid);
+
     // Park might not be present in our list because it was removed in the mean time, so check for that
     if (updatePark != null) {
         updatePark.activationChecked = true;
-        updatePark.activationCount = activations.size;
-        if (activations.size > 0) {
+        updatePark.activationCount = activations.length;
+        if (activations.length > 0) {
             updatePark.activated = true;
-            updatePark.lastActivationDate = moment.utc(activations.get("0").qso_date, "YYYYMMDD");
-            updatePark.lastActivationCallsign = activations.get("0").activeCallsign;
+            updatePark.lastActivationDate = moment.utc(activations[0].qso_date, "YYYYMMDD");
+            updatePark.lastActivationCallsign = activations[0].activeCallsign;
             let actxByMe = 0;
             activations.forEach(activation => {
                 if (myCallsign === activation.activeCallsign) {
-                    if (activation.totalQSOs >= 10) {
-                        updatePark.activatedByMe = true;
-                        actxByMe = actxByMe + 1;
-                        if (updatePark.lastActivationByMeDate == null) {
-                            updatePark.lastActivationByMeDate = moment.utc(activation.qso_date, "YYYYMMDD");
-                        }
-                    } else {
-                        updatePark.failedActivationsByMe = true;
+                    updatePark.activatedByMe = true;
+                    actxByMe = actxByMe + 1;
+                    if (updatePark.lastActivationByMeDate == null) {
+                        updatePark.lastActivationByMeDate = moment.utc(activation.qso_date, "YYYYMMDD");
                     }
                 }
             });
             updatePark.activationByMeCount = actxByMe;
         }
+
+        // As well as successful activations, we need to check if the user has *un*successful activations because we
+        // have a status colour for this.
+        updatePark.failedActivationsByMe = activationAttempts.some(aa => myCallsign === aa.activeCallsign && aa.totalQSOs < 10);
+
+        // Update the data moel and display.
         parks.set(uid, updatePark);
         updateMapObjects();
     }
